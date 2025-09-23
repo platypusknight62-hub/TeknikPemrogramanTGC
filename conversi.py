d88f3a0f85701f254adb73f6f8bdd731114ac919
platypusknight62-hub <platypusknight62@gmail.com>
2025-09-23 16:26:07 +0700

    commit

---
+++ conversi.py
@@ -0,0 +1,5 @@
+def konversi_celcius_ke_fahrenheit(celcius):
+    return (celcius * 9/5) + 32
+
+def konversi_fahrenheit_ke_celcius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
---
+++ perhitungan_suhu.py
@@ -0,0 +1,7 @@
+import conversi 
+
+celcius = float(input("masukkan suhu dalam celcius: "))
+print("suhu dalam fahrenheit adalah:", conversi.konversi_celcius_ke_fahrenheit(celcius))
+
+fahrenheit = float(input("masukkan suhu dalam fahrenheit: "))
+print("suhu dalam celcius adalah:", conversi.konversi_fahrenheit_ke_celcius(fahrenheit))
